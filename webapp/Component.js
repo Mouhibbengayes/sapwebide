bf508827111b0affcfa0a65e130dae72fc3f6289
Mouhibbengayes <bengayes.mouhamedmouhib@esprit.tn>
2024-04-22 09:26:16 +0100

    First commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("tmpz_app_mouhib.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("tmpz_app_mouhib.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
